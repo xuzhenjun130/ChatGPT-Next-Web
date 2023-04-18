e5bd0d5d416c643bf02e065cd8c4cc17c185ea15
xuzhenjun <412530435@qq.com>
2023-04-18 23:50:03 +0800

    聊天总结优化

--- app/store/app.ts
+++ app/store/app.ts
@@ -531,14 +531,19 @@ export const useChatStore = create<ChatStore>()(
           session.topic === DEFAULT_TOPIC &&
           countMessages(session.messages) >= SUMMARIZE_MIN_LEN
         ) {
-          requestWithPrompt(session.messages, Locale.Store.Prompt.Topic).then(
-            (res) => {
-              get().updateCurrentSession(
-                (session) =>
-                  (session.topic = res ? trimTopic(res) : DEFAULT_TOPIC),
-              );
-            },
+          //不要对会话进行总结，节省token
+          get().updateCurrentSession(
+            (session) =>
+              (session.topic = session.messages[0].content.substring(0, 10)),
           );
+          // requestWithPrompt(session.messages, Locale.Store.Prompt.Topic).then(
+          //   (res) => {
+          //     get().updateCurrentSession(
+          //       (session) =>
+          //         (session.topic = res ? trimTopic(res) : DEFAULT_TOPIC),
+          //     );
+          //   },
+          // );
         }
 
         const config = get().config;
@@ -592,7 +597,7 @@ export const useChatStore = create<ChatStore>()(
             },
           );
         }
-      },
+      }, //summarizeSession end
 
       updateStat(message) {
         get().updateCurrentSession((session) => {
